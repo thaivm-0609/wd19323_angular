a5d732ebd9d78715d865154ec4e0d47f65b1a559
thaivm-0609 <vuongthai95@gmail.com>
2025-03-12 09:11:06 +0700

    feat: install bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
-@Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+@Component({//Meta-data
+  selector: 'app-root', //cách gọi/nhúng component vào trong trang/component khác
+  imports: [RouterOutlet], //khai báo những component sẽ được sử dụng trong component này
+  templateUrl: './app.component.html', //đường dẫn đến file giao diện
+  styleUrl: './app.component.css' //đường dẫn đến file css
 })
-export class AppComponent {
-  title = 'wd19323_angular';
+
+export class AppComponent { //khai báo biến/hàm sẽ sử dụng trong component
+  title = 'thaivm2';
+  listStudents = [
+    {
+      maSv: 'PH12345',
+      ten: 'Thaivm2',
+      nganh: 'Web',
+    },
+    {
+      maSv: 'PH12346',
+      ten: 'Thaivm3',
+      nganh: 'Marketing',
+    },
+    {
+      maSv: 'PH12347',
+      ten: 'Thaivm4',
+      nganh: 'Design',
+    }
+  ]
 }
