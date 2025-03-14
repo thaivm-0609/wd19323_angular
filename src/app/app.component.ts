20de2c488691e3709ccb0d7908beb6d707c101ab
thaivm-0609 <vuongthai95@gmail.com>
2025-03-14 09:15:50 +0700

    feat: data binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 @Component({//Meta-data
   selector: 'app-root', //cách gọi/nhúng component vào trong trang/component khác
-  imports: [RouterOutlet], //khai báo những component sẽ được sử dụng trong component này
+  imports: [RouterOutlet, CommonModule, FormsModule], //khai báo những package/component sẽ được sử dụng trong component này
   templateUrl: './app.component.html', //đường dẫn đến file giao diện
   styleUrl: './app.component.css' //đường dẫn đến file css
 })
@@ -15,16 +16,26 @@ export class AppComponent { //khai báo biến/hàm sẽ sử dụng trong compo
       maSv: 'PH12345',
       ten: 'Thaivm2',
       nganh: 'Web',
+      thi: 3 
     },
     {
       maSv: 'PH12346',
       ten: 'Thaivm3',
       nganh: 'Marketing',
+      thi: 5
     },
     {
       maSv: 'PH12347',
       ten: 'Thaivm4',
       nganh: 'Design',
+      thi: 10
     }
   ]
+
+  count: number = 0;
+  countClick(): number {
+    return this.count++;
+  }
+
+  name: string = '';
 }
